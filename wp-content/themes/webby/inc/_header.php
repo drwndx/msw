a6e0ba9e3752301385bd6e403984480195b4a783
Unknown <drwndx@gmail.com>
2018-12-27 03:22:45 -0700

    fixing hamburger menu

--- wp-content/themes/webby/inc/_header.php
+++ wp-content/themes/webby/inc/_header.php
@@ -81,7 +81,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col">
-			<nav class="navbar navbar-expand-lg">
+			<nav class="navbar navbar-expand-lg navbar-dark">
 				<a class="navbar-brand" href="/">MSW2019</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
